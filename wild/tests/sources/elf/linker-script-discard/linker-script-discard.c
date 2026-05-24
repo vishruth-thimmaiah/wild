11cabf801061b792d245866dfeb85266dd4bbbaa
vishruth-thimmaiah <vishruththimmaiah@gmail.com>
2026-05-25 00:57:31 +0530

    tests
    Signed-off-by: vishruth-thimmaiah <vishruththimmaiah@gmail.com>

---
+++ wild/tests/sources/elf/linker-script-discard/linker-script-discard.c
@@ -0,0 +1,14 @@
+//#Mode:dynamic
+//#RunEnabled:false
+//#LinkArgs:-shared -z now -T ./linker-script-discard.ld
+//#DiffIgnore:section.got
+//#DiffIgnore:section.riscv.attributes
+//#DiffIgnore:segment.RISCV_ATTRIBUTES.*
+// GNU ld emits `.riscv.attributes`, but Wild does not
+//#DiffIgnore:riscv_attributes.*
+//#DiffIgnore:segment.LOAD.RX.alignment
+//#DiffIgnore:segment.LOAD.RWX.alignment
+//#DoesNotContain:DISCARD
+//#DoesNotContain:.text
+
+int foo() { return 0; }
