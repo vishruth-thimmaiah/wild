dedacd9354ff1a520a675c7af998d4db847a34be
vishruth-thimmaiah <vishruththimmaiah@gmail.com>
2026-06-06 12:18:42 +0530

    fix: update tests to account for RISCV_ATTRIBUTE
    Signed-off-by: vishruth-thimmaiah <vishruththimmaiah@gmail.com>

--- wild/tests/sources/elf/linker-script-phdrs/linker-script-phdrs.c
+++ wild/tests/sources/elf/linker-script-phdrs/linker-script-phdrs.c
@@ -2,7 +2,7 @@
 //#Mode:dynamic
 //#RunEnabled:false
 //#CompArgs:-fPIE -fPIC
-//#LinkArgs:-shared -z now -T ./linker-script-phdrs.ld
+//#LinkArgs:-shared -z now -T ./linker-script-phdrs.ld --defsym=is_riscv=0
 //#DiffIgnore:section.got
 //#ExpectProgramHeader:LOAD flags=RX,sections=[.text]
 //#ExpectProgramHeader:LOAD flags=RW,sections=[*]
@@ -13,10 +13,12 @@
 //#NoProgramHeader:GNU_STACK
 //#NoProgramHeader:GNU_RELRO
 //#NoProgramHeader:GNU_PROPERTY
+//#SkipArch:riscv64
 
 //#Config:riscv:default
 //#Arch:riscv64
 //#ExpectProgramHeader:RISCV_ATTRIBUTES flags=R,sections=[.riscv.attributes]
+//#LinkArgs:-shared -z now -T ./linker-script-phdrs.ld --defsym=is_riscv=1
 
 const char message[] = "Hello PHDRS";
 
