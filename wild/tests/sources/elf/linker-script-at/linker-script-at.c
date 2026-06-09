afec152158382adf59aeb989c7ba61ebec4de740
Vishruth Thimmaiah <vishruththimmaiah@gmail.com>
2026-06-10 02:18:49 +0530

    Revert "fix: do not check flags of segments"
    This reverts commit 020b2607a59608a8e3e9c188d754234f7d48a09a.

--- wild/tests/sources/elf/linker-script-at/linker-script-at.c
+++ wild/tests/sources/elf/linker-script-at/linker-script-at.c
@@ -2,8 +2,8 @@
 //#Object:runtime.c
 // RISC-V: BFD complains about missing __global_pointer$ (defined in the default linker script)
 //#SkipArch:riscv64
-//#ExpectProgramHeader:LOAD sections=[.text,*],paddr=0x200000,vaddr=0x100000
-//#ExpectProgramHeader:LOAD sections=[.data,*],paddr=0x300000,vaddr=0x200000
+//#ExpectProgramHeader:LOAD flags=RX,sections=[.text,*],paddr=0x200000,vaddr=0x100000
+//#ExpectProgramHeader:LOAD flags=RW,sections=[.data,*],paddr=0x300000,vaddr=0x200000
 
 #include "../common/runtime.h"
 
