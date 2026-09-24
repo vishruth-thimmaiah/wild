a4db3b4329d5fe31dba38c18ae3151e3bf305ff2
Vishruth Thimmaiah <vishruththimmaiah@gmail.com>
2026-09-25 02:58:15 +0530

    more tests
    Signed-off-by: Vishruth Thimmaiah <vishruththimmaiah@gmail.com>

--- wild/tests/sources/elf/orphan-handling/orphan-handling.c
+++ wild/tests/sources/elf/orphan-handling/orphan-handling.c
@@ -8,7 +8,7 @@
 //#DiffIgnore:segment.LOAD.RWX.alignment
 
 //#AbstractConfig:default:common
-//#LinkerScript:linker-script-orphan-handling.ld
+//#LinkerScript:orphan-handling.ld
 
 //#Config:orphan-place:default
 //#LinkArgs:--orphan-handling=place
@@ -16,12 +16,12 @@
 
 //#Config:orphan-warn:default
 //#LinkArgs:--orphan-handling=warn
-//#ExpectWarning:orphan section .\.orphan_section' from .*linker-script-orphan-handling\.c\.o' being placed in section .\.orphan_section.
+//#ExpectWarning:orphan section .\.orphan_section' from .*orphan-handling\.c\.o' being placed in section .\.orphan_section.
 
 //#Config:orphan-error:default
 //#CompArgs:-fno-asynchronous-unwind-tables -fno-unwind-tables
 //#LinkArgs:--orphan-handling=error
-//#ExpectError:unplaced orphan section .\.orphan_section' from .*linker-script-orphan-handling\.c\.o'
+//#ExpectError:unplaced orphan section .\.orphan_section' from .*orphan-handling\.c\.o'
 
 //#Config:orphan-discard:default
 //#LinkArgs:--orphan-handling=discard
@@ -61,6 +61,26 @@
 //#LinkArgs:--no-gc-sections --orphan-handling=discard
 //#ExpectSection:.eh_frame
 
+//#AbstractConfig:filtered-eh-frame:common
+//#LinkerScript:orphan-handling-filtered-ehframe.ld
+//#CompArgs:-fasynchronous-unwind-tables -DNO_ORPHAN_VAR
+
+//#Config:filtered-eh-frame-place:filtered-eh-frame
+//#LinkArgs:--no-gc-sections --orphan-handling=place
+//#ExpectSection:.eh_frame
+
+//#Config:filtered-eh-frame-warn:filtered-eh-frame
+//#LinkArgs:--no-gc-sections --orphan-handling=warn
+//#ExpectWarning:orphan section .\.eh_frame'
+
+//#Config:filtered-eh-frame-error:filtered-eh-frame
+//#LinkArgs:--no-gc-sections --orphan-handling=error
+//#ExpectError:unplaced orphan section .\.eh_frame'
+
+//#Config:filtered-eh-frame-discard:filtered-eh-frame
+//#LinkArgs:--no-gc-sections --orphan-handling=discard
+//#NoSection:.eh_frame
+
 //#AbstractConfig:partial:common
 //#RunEnabled:false
 //#DiffEnabled:false
@@ -76,6 +96,26 @@
 //#Config:orphan-partial-error:partial
 //#LinkArgs:-r --orphan-handling=error
 //#ExpectError:unplaced orphan section .\.orphan_section'
+
+//#Config:orphan-partial-discard:partial
+//#LinkArgs:-r --orphan-handling=discard
+//#NoSection:.orphan_section
+
+//#Config:no-script-place:common
+//#LinkArgs:--orphan-handling=place
+//#ExpectSection:.orphan_section
+
+//#Config:no-script-warn:common
+//#LinkArgs:--orphan-handling=warn
+//#ExpectWarning:orphan section .\.orphan_section'
+
+//#Config:no-script-error:common
+//#LinkArgs:--orphan-handling=error
+//#ExpectError:unplaced orphan section .\.orphan_section'
+
+//#Config:no-script-discard:common
+//#LinkArgs:--orphan-handling=discard
+//#NoSection:.orphan_section
 
 #include "../common/runtime.h"
 
